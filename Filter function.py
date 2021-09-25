f29d83ac481c2437c098415ddd0ee5a81684b4a5
Hassan <hassansmallah@gmail.com>
2021-09-25 14:53:45 +0300

    Filter function.py

---
+++ Filter function.py
@@ -0,0 +1,16 @@
+# Filter function
+
+def add7(x):
+    return x + 7
+
+
+def is_odd(x):
+    return x % 2 != 0
+
+
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+b = list(filter(is_odd, a))  # filter list
+c = list(map(add7, b))  # apply map to the list
+
+print(c)
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Intermediate Python Tutorial
 - Optional Parameters
 - Static and Class Methods
 - Map function
+- Filter function
